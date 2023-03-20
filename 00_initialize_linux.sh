09e107c9e3376f25d51d14e255679233f0a95cca
JoSH@CoolUbuntu <coolwind@hotmail.co.kr>
2023-03-20 11:18:47 +0900

    make installing git folder separately

--- 00_initialize_linux.sh
+++ 00_initialize_linux.sh
@@ -4,5 +4,4 @@ sudo apt-get update -y && sudo apt-get install -q -y \
 	wget \
 	build-essential gdb clang lldb g++-10 \
 	python3 \
-	git \
 && sudo apt-get clean && sudo rm -rf /var/lib/apt/lists/*
--- 01_set_git_config.sh
+++
@@ -1,2 +0,0 @@
-git config --global user.email "your_email@example.com"
-git config --global user.name "your_name"
